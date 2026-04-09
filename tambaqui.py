18064378115f15a8c12124db1f4ac7ecaafe7f4e
Daniel Rond de Jesus dos Santos <166262041+DanielRond@users.noreply.github.com>
2026-04-09 08:55:59 -0300

    questoes completas 2

--- tambaqui.py
+++ tambaqui.py
@@ -1,2 +1,22 @@
-p1,p2,x,c = map(float,input()split())
-print(f"R$ {:.2f} Banca {}")
+p1, p2, x, c = map(float, input().split())
+pix = (p1 + p2) * (1 - x/100) 
+cartao = (p1 + p2) * (1 + c/100)
+total1 = min(pix,cartao)
+
+p1, p2, x, c = map(float, input().split())
+pix = (p1 + p2) * (1 - x/100) 
+cartao = (p1 + p2) * (1 + c/100)
+total2 = min(pix,cartao)
+
+p1, p2, x, c = map(float, input().split())
+pix = (p1 + p2) * (1 - x/100) 
+cartao = (p1 + p2) * (1 + c/100)
+total3 = min(pix,cartao)
+
+minimo = min(total1, total2, total3)
+if minimo == total1:
+    print(f"R$ {total1:.2f} Banca 1")
+elif minimo == total2:
+    print(f"R$ {total2:.2f} Banca 2")
+else:
+    print(f"R$ {total3:.2f} Banca 3")
