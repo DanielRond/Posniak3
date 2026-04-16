01552051e52af9e12b9203cb86b45893dad0941f
Daniel Rond de Jesus dos Santos <166262041+DanielRond@users.noreply.github.com>
2026-04-16 11:03:12 -0300

    atv 3

---
+++ Atividade 3/tira.py
@@ -0,0 +1,6 @@
+x,y = map(int,input().split())
+#(0,0) e (432, 468)
+if (x>=0 and x<=432) and (y>=0 and y<=468):
+    print("dentro")
+else:
+    print("fora")
---
+++ vacina.py
@@ -0,0 +1,7 @@
+a,b = map(float,input().split())
+if (a<8) or (b<8):
+    print("bom jesus")
+elif(a>8) or (b>8): #Akira n˜ao acordar antes das 8h e Akir´o tamb´em n˜ao acordar antes das 8h
+    print("bom jesus")
+else:
+    print("cruzeiro")
